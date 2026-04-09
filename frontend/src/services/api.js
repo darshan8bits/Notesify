cf946fdc8f0434f20477c90d2e0c3e10a421c2bc
Darshan Gupta <darshangupta78421@gmail.com>
2026-04-09 16:34:40 +0530

    fixed routing issues

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://notesify-2.onrender.com/api",
+  baseURL: "https://notesify-2.onrender.com",
 });
 
 // attach token (NO Bearer)
